ffa18848d2781361ba90e633fd85f914782c7a39
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-05-11 15:29:25 +0600

    Updated: Questions getter

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,12 @@ const _ = require('lodash');
 admin.initializeApp(functions.config().firestore);
 admin.firestore().settings({ timestampsInSnapshots: true });
 
+async function getQuestions(eventId) {
+    const snapshot = await admin.firestore().collection('events').doc(eventId).get();
+    return snapshot.data().questions;
+}
 
 exports.createEventRooms = functions.https.onRequest(async (req, res) => {
-    res.send('Hello World');
+    let questions = await getQuestions('test3');
+    res.send(questions)
 })
